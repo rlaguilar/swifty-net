e80fb760215a44133cbd4544fcdc6cc6cd7c6f03
Reynaldo Aguilar <reynaldo@canva.com>
2022-07-24 23:40:11 +1000

    Add tests for HTTPMethod

---
+++ Tests/SwiftyNetTests/HTTPMethodTests.swift
@@ -0,0 +1,21 @@
+import Foundation
+import XCTest
+@testable import SwiftyNet
+
+final class HTTPMethoTests: XCTestCase {
+    func testName_WhenMethodIsGet_ReturnsGET() {
+        let method = HTTPMethod.get
+
+        let name = method.name
+
+        XCTAssertEqual(name, "GET")
+    }
+
+    func testName_WhenMethodIsPost_ReturnsPOST() {
+        let method = HTTPMethod.post("")
+
+        let name = method.name
+
+        XCTAssertEqual(name, "POST")
+    }
+}
